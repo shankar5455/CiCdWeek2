8e46dd7599db2954b7422197ea928070be5a93fa
shankar5455 <pillyigshankar@gmail.com>
2025-07-26 21:27:37 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CiCdWeek2/',
   plugins: [react()],
 })
